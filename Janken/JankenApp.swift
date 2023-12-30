0fee0b517c3e94e8562a8a3bfbd49f007788e8db
i-qbu <iqbu.lab@gmail.com>
2023-12-31 07:30:42 +0900

    Initial Commit

---
+++ Janken/JankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenApp.swift
+//  Janken
+//
+//  Created by 渡辺大智 on 2023/12/31.
+//
+
+import SwiftUI
+
+@main
+struct JankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
